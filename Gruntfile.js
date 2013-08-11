34c539e1b1fc762e183397062a7d3da07a2834cb
yukihir0 <yukihiro.cotori@gmail.com>
2013-08-11 11:46:01 +0900

    add grunt settings

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function (grunt) {
+  'use strict';
+  var pkg = grunt.file.readJSON('package.json');
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+    less: {
+      development: {
+        options: {
+          paths: ["less"]
+        },
+        files: {
+          "dist/tachikoma.css": "tachikoma.less"
+        }
+      }
+    },
+    connect: {
+      livereload: {
+        options: {
+          port: 9001
+        }
+      }
+    },
+    watch: {
+      html: {
+        files: 'views/*.html',
+        tasks: [],
+        options: {
+          livereload: true,
+          nospawn: true
+        }
+      },
+      less: {
+        files: '*.less',
+        tasks: ['less:development'],
+        options: {
+          livereload: true,
+          nospawn: true
+        }
+      }
+    }
+  });
+  var taskName;
+  for(taskName in pkg.devDependencies) {
+    if(taskName.substring(0, 6) == 'grunt-') {
+      grunt.loadNpmTasks(taskName);
+    }
+  }
+  grunt.registerTask('live', ['connect', 'watch']);
+  grunt.registerTask('default', ['less:development', 'live']);
+};
